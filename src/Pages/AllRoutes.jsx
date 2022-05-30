c87bb8adec75bb459b1f2cff4158b70869edc122
manjeetsingh100001 <manjeetsingh100001@yahoo.com>
2022-05-30 17:28:18 +0530

    home

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -3,13 +3,15 @@ import Dashboard from "./Dashboard"
 import RegisterPageOne from "./RegisterPageOne" 
 import RegisterPageTwo from "./RegisterPageTwo"
 import PrivateRoute from "./PrivateRoute"
-
+import {Link} from "react-router-dom"
 
 function AllRoutes(){
     return(
         <>
         <Routes>
-        <Route path="/" element={<h1> select the respective route for navigation</h1>}/>
+        <Route path="/" element={<div><h1> select the respective route for navigation</h1>
+        <button>  <Link to="/register/one">Register one </Link></button>
+        </div>}/>
             <Route path="/dashboard" element={
             <PrivateRoute>
             <Dashboard/>
